25072e3167aa8c9cd7a395f74271677a73d598e8
SyedAli0902992 <60199386+SyedAli0902992@users.noreply.github.com>
2020-03-17 14:00:13 +0530

    changes

---
+++ src/test/java/jenkins/AppTest.java
@@ -0,0 +1,17 @@
+package jenkins;
+
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+   
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
